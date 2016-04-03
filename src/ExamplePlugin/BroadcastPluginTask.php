df6f58bfad5d89d30408f660ab051478734e4cfc
TheLucyClub Github Team <thelucyclub@gmail.com>
2016-04-03 17:56:12 -0400

    fur me

--- src/ExamplePlugin/BroadcastPluginTask.php
+++ src/ExamplePlugin/BroadcastPluginTask.php
@@ -5,10 +5,11 @@ namespace ExamplePlugin;
 use pocketmine\Player;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\Server;
+use pocketmine\utils\TextFormat as C;
 
 class BroadcastPluginTask extends PluginTask{
 
 	public function onRun($currentTick){
-		Server::getInstance()->broadcastMessage("[ExamplePlugin] I've run on tick " . $currentTick);
+		Server::getInstance()->broadcastTip(C::GOLD."Welcome to ".C::AQUA."Diamond ".C::GREEN."Realms ".C::YELLOW."P".C::RED"E\n".C::GRAY."-=[ ".C::YELLOW.count($this->getServer()->getOnlinePlayers).C::WHITE. "/ "C::YELLOW.$this->getServer()->getMaxPlyers().C::GRAY." ]=-");
 	}
 }
